e7fa03d58f21e1fa27a5106b63d4a2bf85cd65d8
emipa606 <emil.offermo@litet.net>
2021-08-03 10:57:45 +0200

    First publish

--- README.md
+++ README.md
@@ -1,1 +1,33 @@
 # GiveUpYourBuilding
+
+![Image](https://i.imgur.com/WAEzk68.png)
+
+Update of LingLuos mod
+https://steamcommunity.com/sharedfiles/filedetails/?id=1973531614
+
+![Image](https://i.imgur.com/7Gzt3Rg.png)
+
+	
+![Image](https://i.imgur.com/NOW7jU1.png)
+
+Give up your buildings so that enemies won&apos;t attack them.
+
+The function of this mod is the same as &quot;Not Mine- Unclaim Structures&quot;, but he makes these buildings belong to insect, and I make these buildings not belong to anyone, which will be different in the face of insect
+
+languages:
+Simplified Chinese;
+English.
+
+![Image](https://i.imgur.com/Rs6T6cr.png)
+
+
+
+-  See if the the error persists if you just have this mod and its requirements active.
+-  If not, try adding your other mods until it happens again.
+-  Post your error-log using https://steamcommunity.com/workshop/filedetails/?id=818773962]HugsLib and command Ctrl+F12
+-  For best support, please use the Discord-channel for error-reporting.
+-  Do not report errors by making a discussion-thread, I get no notification of that.
+-  If you have the solution for a problem, please post it to the GitHub repository.
+
+
+
---
+++ Source/GiveUpYourBuilding/Designator_GiveUpBuilding.cs
@@ -0,0 +1,81 @@
+﻿using System.Linq;
+using RimWorld;
+using UnityEngine;
+using Verse;
+
+namespace ZeroTech
+{
+    // Token: 0x02000002 RID: 2
+    public class Designator_GiveUpBuilding : Designator
+    {
+        // Token: 0x06000002 RID: 2 RVA: 0x00002064 File Offset: 0x00000264
+        public Designator_GiveUpBuilding()
+        {
+            defaultLabel = "GiveUpBuilding".Translate();
+            defaultDesc = "GiveUpBuildingDesc".Translate();
+            icon = ContentFinder<Texture2D>.Get("UI/Designators/GiveUpBuilding");
+            soundDragSustain = SoundDefOf.Designate_DragStandard;
+            soundDragChanged = SoundDefOf.Designate_DragStandard_Changed;
+            useMouseIcon = true;
+            soundSucceeded = SoundDefOf.Designate_Claim;
+            hotKey = KeyBindingDefOf.Misc1;
+        }
+
+        // Token: 0x17000001 RID: 1
+        // (get) Token: 0x06000001 RID: 1 RVA: 0x00002050 File Offset: 0x00000250
+        public override int DraggableDimensions => 2;
+
+        // Token: 0x06000003 RID: 3 RVA: 0x000020E8 File Offset: 0x000002E8
+        public override AcceptanceReport CanDesignateCell(IntVec3 c)
+        {
+            if (!c.InBounds(Map))
+            {
+                return false;
+            }
+
+            if (c.Fogged(Map))
+            {
+                return false;
+            }
+
+            if (!(from t in c.GetThingList(Map)
+                where CanDesignateThing(t).Accepted
+                select t).Any())
+            {
+                return "MessageNoCanGiveUpBuilding".Translate();
+            }
+
+            return true;
+        }
+
+        // Token: 0x06000004 RID: 4 RVA: 0x00002174 File Offset: 0x00000374
+        public override void DesignateSingleCell(IntVec3 c)
+        {
+            var thingList = c.GetThingList(Map);
+            foreach (var thing in thingList)
+            {
+                var accepted = CanDesignateThing(thing).Accepted;
+                if (accepted)
+                {
+                    DesignateThing(thing);
+                }
+            }
+        }
+
+        // Token: 0x06000005 RID: 5 RVA: 0x000021D4 File Offset: 0x000003D4
+        public override AcceptanceReport CanDesignateThing(Thing t)
+        {
+            return t is Building building && building.Faction == Faction.OfPlayer;
+        }
+
+        // Token: 0x06000006 RID: 6 RVA: 0x00002208 File Offset: 0x00000408
+        public override void DesignateThing(Thing t)
+        {
+            t.SetFaction(null);
+            foreach (var intVec3 in t.OccupiedRect())
+            {
+                FleckMaker.ThrowMetaPuffs(new TargetInfo(intVec3, Map));
+            }
+        }
+    }
+}
